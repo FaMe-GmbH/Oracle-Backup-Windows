d99f8bd9c28b2ace675bb7b4561605b6d349f15f
M. Eisenhardt <m.eisenhardt@fame-online.de>
2021-05-10 00:43:01 +0200

    find_system_pwd.cmd: changed regular pattern and Perl script now eavls ORACLE_SID as well

--- find_system_pwd.cmd
+++ find_system_pwd.cmd
@@ -1,2 +1,2 @@
 @echo off
-findstr /R "^%ORACLE_SID%"  backup.conf | perl -ne "chomp; @f=split(/:/, $_); if (@f >= 3 && $f[2] =~ m!system/(.+)!i) { print $1; }"
+findstr /R "^%ORACLE_SID%\s*:"  backup.conf | perl -ne "chomp; @f=split(/:/, $_); if (@f >= 3 && $f[0] eq qq(%ORACLE_SID%) && $f[2] =~ m!system/(.+)!i) { print $1; }"
